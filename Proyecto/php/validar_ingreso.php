e434fa804b28683f8aee2a86e6a1f3074ba36670
Elitrea <65686493+Elitrea@users.noreply.github.com>
2023-03-23 12:55:12 -0600

    actualizacion validar_usuario

--- Proyecto/php/validar_ingreso.php
+++ Proyecto/php/validar_ingreso.php
@@ -10,11 +10,22 @@ $_SESSION['correo']=$usuario;
 include('connect.php');
 
 
-
-$consulta="SELECT * FROM usuario WHERE correo= '$usuario' and clave ='$contrasena'";
+$consulta="SELECT * FROM usuarios WHERE correo= '$usuario' and clave ='$contrasena'";
 $resultado=mysqli_query($conex,$consulta);
 $datos=mysqli_fetch_array($resultado);
 $filas=mysqli_num_rows($resultado);
+
+
+if($filas)
+{
+    header("location:../html/readReceta.html");
+}
+else{
+    echo "<script>
+        alert('Nombre de usuario o contraseña incorrectos');
+        </script>";
+        include("../html/index.html");
+}
 
 
 mysqli_free_result($resultado);
