d899771889e1f08f2115f8d5f3831da074d9ef88
AlejandroTC <aletamayo-02@hotmail.com>
2023-03-23 00:17:44 -0600

    autocomplete, falta crear si no esta y dar estilo

---
+++ Proyecto/php/ingredientes.php
@@ -0,0 +1,30 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "recetasDB";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // Establecer el modo de error PDO a excepción
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  
+    // Consulta SQL para recuperar los datos
+    $sql = "SELECT nombre FROM ingredientes";
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    
+  
+    // Recuperar los resultados y convertirlos en un objeto JSON
+    $resultados = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $json = json_encode($resultados);
+  
+    // Enviar la respuesta como JSON
+    header('Content-Type: application/json');
+   
+    echo $json;
+  
+  } catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+  }
+?>
