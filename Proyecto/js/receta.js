c6431d1c04a6c432ab813b5394b43e0d89f9e002
AlejandroTC <aletamayo-02@hotmail.com>
2023-03-24 15:08:53 -0600

    Guarda los ingredientes en DB

--- Proyecto/js/receta.js
+++ Proyecto/js/receta.js
@@ -1,38 +1,47 @@
 let ADDrecipe = document.getElementById("guardarreceta");
-ADDrecipe.onclick = function(){
-    let RDuration = document.getElementById("rduracion").value;
-    let RPortion = document.getElementById("rporcion").value;
-    let RTime = document.getElementById("rtiempo").value;
-    let RType = document.getElementById("rtipo").value;
-    let RImg = document.getElementById("rimg").value;
-    //Seccionar las consultas e insert
-        //iniciando con la tabla Receta
-        
+ADDrecipe.onclick = function () {
+  let RDuration = document.getElementById("rduracion").value;
+  let RPortion = document.getElementById("rporcion").value;
+  let RTime = document.getElementById("rtiempo").value;
+  let RType = document.getElementById("rtipo").value;
+  let RImg = document.getElementById("rimg").value;
+  //Seccionar las consultas e insert
+		//revisamos ingredientes para insertar los que no existan
+		const table = document.querySelector('#listaingredientes'); // Obtener la tabla
+		const tds = table.querySelectorAll("td:not(.col-md-1)"); // Obtener todos los td que no tienen clase "col-md-1"
+		const texts = Array.from(tds).map((td) => td.textContent); // Obtener la cadena de texto de cada td y guardarlos en un array
+		
+		//Iterar el array y separarlo
+		for(let i=0; i < texts.length; i++){
+			const fila = texts[i];
+			const split = fila.split(' ')
+			const indiceDe = split.indexOf("de");
+			const ingrediente = split.slice(indiceDe + 1).join(" ");
+			
+			existe(ingrediente);
+		}
+		
+		
+  //iniciando con la tabla Receta
+}
 
-    //agregar valores a la tabla de listado de ingredientes con el icono para eliminar
-    let explicacion = NStep + " " + Step + " " + Stepimg;  //Encadenamos para formar el texto a plasmar
-    const tr=document.createElement("tr");              //Creamos el TR
-    const tdRemove=document.createElement("td");        //Creamos el TD
-    tdRemove.className = "col-md-1 col-sm-1";           //Damos clase al TD
-    const icon = document.createElement('i');           //Creamos el i
-    icon.classList.add('fa', 'fa-times-circle-o');      //Damos clase al i
-    icon.style.color = 'rgb(255, 0, 89)';               //Asignamos color al i
-    icon.style.textAlign = 'right';                     //Asignamos posicion al i
-    icon.onclick=eliminarFila;                          //Asignamos evento o funcion al clickar
-    const tdtext=document.createElement("td");          //Creamos el TD de texto
-    tdtext.className = "col-md-11 col-sm-11";           //Damos clase al TD de texto
-    let txt = document.createTextNode(explicacion);    //Inicializamos un nodo con el texto a plasmar
-    tdtext.appendChild(txt);                            //Agregamos el textro al TD
-    tdRemove.appendChild(icon);                         //Agregamos el icono al TD
-    tr.appendChild(tdRemove);                           //Agregamos el TD al TR
-    tr.appendChild(tdtext);                             //Agregamos el TD al TR
-    console.log(explicacion);
-    const tbody=document.getElementById("procedimiento").querySelector("tbody").appendChild(tr);  //Agragamos al TBody ambos TR
+function existe(ingrediente){
+	fetch('../php/ingredientes.php')
+	.then(response => response.json())
+	.then(data => {
+			let ingre = data;
+			let nombres = ingre.map(obj => obj.nombre);
+			if(!nombres.includes(ingrediente)){
+					fetch('../php/insertar_ingrediente.php', {
+							method: 'POST',
+							headers: {
+									'Content-Type': 'application/x-www-form-urlencoded'
+							},
+							body: 'sql=INSERT INTO ingredientes (nombre) VALUES ("' + ingrediente + '")'
+					})
+					.then(response => console.log('Ingrediente insertado en la base de datos.'))
+					.catch(error => console.error(error));
+			}
+	})
+	.catch(error => console.error(error));
 }
-//Para eliminar
-function eliminarFila(){
-    const tr = this.closest("tr");
-    tr.remove();
-    console.log("Se clico");
-}
-    
