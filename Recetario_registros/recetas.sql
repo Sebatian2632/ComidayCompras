773777738ee9f269f96e6f105b12a6d9138db69d
AnaRebo15 <anapaorebo15@gmail.com>
2023-04-25 09:02:39 -0600

    EjemplosRegistros
    Deben de crear la BD desde 0, y después correr los SQL para que las relaciones queden correctas.

---
+++ Recetario_registros/recetas.sql
@@ -0,0 +1,38 @@
+INSERT INTO recetas (nombre, duracion, tiempo_comida, tiempo_receta, porciones, imagen, calificacion, Usuarios_correo)
+VALUES ('Pancakes de Arándanos', '30 minutos', 'desayuno', 'principal', '4', null, 5, 'juanperez1234@gmail.com'),   --1
+       ('Ensalada César', '15 minutos', 'comida', 'entrada', '2', null, 4, 'juanperez1234@gmail.com'),              --2
+       ('Sopa de Tomate', '40 minutos', 'comida', 'entrada', '6', null, 3, 'juanperez1234@gmail.com'),              --3
+       ('Smoothie de Frutas', '5 minutos', 'merienda', 'bebida', '1', null, 5, 'juanperez1234@gmail.com'),          --4
+       ('Lomo de Cerdo al Horno', '120 minutos', 'cena', 'principal', '6', null, 4, 'juanperez1234@gmail.com'),     --5
+       --------------------------------------------------------------------------------------------------------
+       ('Pastel de Chocolate', '90 minutos', 'postre', 'postre', '8', null, 4, 'juanperez1234@gmail.com'),          --6
+       ('Tacos de Carnitas', '2 horas', 'comida', 'principal', '4', null, 5, 'juanperez1234@gmail.com'),            --7
+       ('Ensalada de Quinoa', '20 minutos', 'almuerzo', 'ensalada', '3', null, 4, 'juanperez1234@gmail.com'),       --8
+       ('Pasta Alfredo con Pollo', '45 minutos', 'comida', 'principal', '6', null, 3, 'juanperez1234@gmail.com'),   --9
+       ('Smoothie Verde Detox', '10 minutos', 'desayuno', 'bebida', '1', null, 5, 'juanperez1234@gmail.com'),       --10
+       --------------------------------------------------------------------------------------------------------
+       ('Pizza Margherita', '45 minutos', 'cena', 'principal', '8', null, 4, 'juanperez1234@gmail.com'),            --11
+       ('Tarta de Manzana', '1 hora 30 minutos', 'postre', 'postre', '6', null, 4, 'juanperez1234@gmail.com'),      --12
+       ('Ceviche de Camarón', '20 minutos', 'comida', 'entrada', '4', null, 0, 'juanperez1234@gmail.com'),          --13
+       ('Hamburguesa de Pollo', '30 minutos', 'comida', 'principal', '4', null, 3, 'juanperez1234@gmail.com'),      --14
+       ('Sopa de Lentejas', '50 minutos', 'comida', 'entrada', '6', null, 4, 'juanperez1234@gmail.com'),        --15
+       --------------------------------------------------------------------------------------------------------
+       ('Enchiladas Verdes', '1 hora', 'cena', 'principal', '4', null, 4, 'mariarod9876@gmail.com'),               --16
+       ('Pastel de Zanahoria', '1 hora 15 minutos', 'postre', 'postre', '10', null, 3, 'mariarod9876@gmail.com'),  --17
+       ('Ensalada de Atún', '15 minutos', 'almuerzo', 'ensalada', '2', null, 4, 'mariarod9876@gmail.com'),         --18
+       ('Pasta Carbonara', '25 minutos', 'comida', 'principal', '4', null, 2, 'mariarod9876@gmail.com'),           --19
+       ('Smoothie de Mango', '5 minutos', 'merienda', 'bebida', '1', null, 5, 'mariarod9876@gmail.com'),        --20
+       --------------------------------------------------------------------------------------------------------
+       ('Sushi de Salmón', '30 minutos', 'cena', 'principal', '4', null, 4, 'mariarod9876@gmail.com'),
+       ('Ceviche de Pulpo', '20 minutos', 'comida', 'entrada', '4', null, 5, 'pedrogomez12@gmail.com'),
+       ('Parrillada Argentina', '1 hora 30 minutos', 'cena', 'principal', '6', null, 4, 'pedrogomez12@gmail.com'),
+       ('Lasagna de Carne', '1 hora', 'comida', 'principal', '8', null, 3, 'pedrogomez12@gmail.com'),
+       ('Pollo a la Parmesana', '40 minutos', 'cena', 'principal', '4', null, 4, 'pedrogomez12@gmail.com'),     --25
+       --------------------------------------------------------------------------------------------------------
+       ('Tiramisú', '30 minutos', 'postre', 'postre', '6', null, 4, 'pedrogomez12@gmail.com'),
+       ('Lomo Saltado', '40 minutos', 'cena', 'principal', '4', null, 5, 'pedrogomez12@gmail.com'),
+       ('Tacos de Pescado', '25 minutos', 'cena', 'principal', '4', null, 4, 'pedrogomez12@gmail.com'),
+       ('Risotto de Champiñones', '40 minutos', 'cena', 'principal', '4', null, 4, 'anamartinp@gmail.com'),
+       ('Pizza Hawaiana', '45 minutos', 'cena', 'principal', '8', null, 4, 'anamartinp@gmail.com');             --30
+
+
---
+++ Recetario_registros/recetas_has_ingredientes.sql
@@ -0,0 +1,294 @@
+INSERT INTO recetas_has_ingredientes (Recetas_idRecetas, Ingredientes_idIngredientes, cantidad, unidad_medida)
+VALUES  (1, 145, 2, 'Taza(s)'),             --Harina de trigo
+        (1, 235, 2, 'Cucharada(s)'),        --Azúcar
+        (1, 301, 2, 'Cucharadita(s)'),      --Polvo para hornear
+        (1, 302, 1, 'Cucharadita(s)'),      --Bicarbonato de sodio
+        (1, 303, 0.5, 'Cucharadita(s)'),    --Sal
+        (1, 304, 2, 'Pieza(s)'),            --Huevo
+        (1, 112, 1.5, 'Taza(s)'),           --Leche
+        (1, 248, 1, 'Cucharadita(s)'),      --Extracto de vainilla
+        (1, 84, 2, 'Taza(s)'),              --Arándano
+        (1, 305, 100, 'Gramos'),            --Mantequilla
+
+        (2, 306, 1, 'Pieza(s)'),            --Lechuga romana
+        (2, 307, 0.5, 'Taza(s)'),           --Crutones
+        (2, 117, 0.25, 'Taza(s)'),          --Queso parmesano
+
+        (3, 15, 2, 'Kilogramo(s)'),         --Tomate
+        (3, 7, 1, 'Pieza(s)'),              --Cebolla
+        (3, 308, 2, 'Pieza(s)'),            --Dientes de ajo
+        (3, 1, 1, 'Pieza(s)'),              --Zanahoria
+        (3, 9, 1, 'Pieza(s)'),              --Papa
+        (3, 309, 4, 'Taza(s)'),             --Caldo de verduras
+        (3, 235, 1, 'Cucharadita(s)'),      --Azúcar
+        (3, 303, 1, 'Cucharadita(s)'),      --Sal
+        (3, 310, 0.5, 'Cucharadita(s)'),    --Pimienta negra molida
+        (3, 311, 0.25, 'Cucharadita(s)'),   --Comino molido
+        (3, 312, 0.25, 'Cucharadita(s)'),   --Pimentón
+        (3, 313, 0.25, 'Taza(s)'),          --Aceite de oliva
+        (3, 39, 0.25, 'Taza(s)'),           --Albahaca
+        (3, 117, 0.25, 'Taza(s)'),          --Queso parmesano
+
+        (4, 57, 1, 'Pieza(s)'),             --Plátano
+        (4, 62, 0.5, 'Taza(s)'),            --Fresas
+        (4, 64, 0.5, 'Taza(s)'),            --Mango
+        (4, 314, 0.5, 'Taza(s)'),           --Yogur natural
+        (4, 315, 0.5, 'Taza(s)'),           --Leche de almendra
+        (4, 239, 1, 'Cucharadita(s)'),      --Miel
+
+        (5, 207, 2, 'Kilogramo(s)'),        --Lomo de cerdo
+        (5, 313, 2, 'Cucharada(s)'),        --Aceite de oliva
+        (5, 303, 2, 'Cucharadita(s)'),      --Sal
+        (5, 310, 1, 'Cucharadita(s)'),      --Pimienta negra molida
+        (5, 41, 2, 'Cucharadita(s)'),       --Romero
+        (5, 45, 2, 'Cucharadita(s)'),       --Tomillo
+        (5, 208, 4, 'Pieza(s)'),            --Dientes de ajo
+        (5, 316, 1, 'Taza(s)'),             --Caldo de pollo
+        (5, 285, 0.5, 'Taza(s)'),           --Vino blanco
+
+        (6, 245, 200, 'Gramos'),            --Chocolate oscuro
+        (6, 317, 200, 'Gramos'),            --Mantequilla sin sal
+        (6, 235, 200, 'Gramos'),            --Azúcar
+        (6, 304, 4, 'Pieza(s)'),            --Huevo
+        (6, 248, 1, 'Cucharadita(s)'),      --Extracto de vainilla
+        (6, 319, 200, 'Gramos'),            --Harina
+        (6, 318, 1, 'Cucharadita(s)'),      --Levadura en polvo
+        (6, 303, 0.5, 'Cucharadita(s)'),    --Sal
+
+        (7, 213, 1, 'Kilogramo(s)'),        --Carnitas de cerdo
+        (7, 320, 1, 'Cucharada(s)'),        --Manteca
+        (7, 303, 1, 'Cucharadita(s)'),      --Sal
+        (7, 310, 1, 'Cucharadita(s)'),      --Pimienta negra molida
+        (7, 311, 1, 'Cucharadita(s)'),      --Comino molido
+        (7, 40, 1, 'Cucharadita(s)'),       --Orégano
+        (7, 321, 1, 'Pieza(s)'),            --Hoja de laurel
+        (7, 58, 1, 'Pieza(s)'),             --Naranja
+        (7, 276, 2, 'Taza(s)'),             --Agua
+        (7, 322, 20, 'Pieza(s)'),           --Tortilla de maíz
+
+        (8, 323, 1, 'Taza(s)'),             --Quinoa cocida
+        (8, 13, 1, 'Pieza(s)'),             --PeAlbahacapino
+        (8, 5, 1, 'Pieza(s)'),              --Pimiento rojo
+        (8, 1, 1, 'Pieza(s)'),              --Zanahoria
+        (8, 7, 0.5, 'Pieza(s)'),            --Cebolla
+        (8, 324, 0.25, 'Taza(s)'),          --Cilantro picado
+        (8, 68, 1, 'Pieza(s)'),             --Limón
+        (8, 313, 2, 'Cucharada(s)'),        --Aceite de oliva
+
+        (9, 325, 500, 'Gramos'),            --Pasta
+        (9, 178, 500, 'Gramos'),            --Pechuga de pollo
+        (9, 317, 1, 'Taza(s)'),             --Mantequilla sin sal
+        (9, 114, 2, 'Taza(s)'),             --Crema
+        (9, 117, 2, 'Taza(s)'),             --Queso parmesano
+        (9, 308, 4, 'Pieza(s)'),            --Dientes de ajo
+        (9, 253, 0.5, 'Cucharadita(s)'),    --Nuez moscada
+
+        (10, 3, 0.5, 'Taza(s)'),            --Espinacas
+        (10, 13, 0.5, 'Taza(s)'),           --Pepino
+        (10, 63, 0.5, 'Taza(s)'),           --Piña
+        (10, 57, 0.5, 'Pieza(s)'),          --Plátano
+        (10, 28, 1, 'Cucharada(s)'),        --Jengibre
+        (10, 68, 0.5, 'Pieza(s)'),          --Limón
+        (10, 326, 0.5, 'Taza(s)'),          --Agua de coco
+
+        (11, 145, 500, 'Gramos'),           --Harina de trigo
+        (11, 276, 325, 'Mililitros'),       --Agua
+        (11, 303, 10, 'Gramos'),            --Sal
+        (11, 235, 10, 'Gramos'),            --Azúcar
+        (11, 318, 10, 'Gramos'),            --Levadura
+        (11, 15, 800, 'Gramos(s)'),         --Tomate
+        (11, 308, 2, 'Pieza(s)'),           --Dientes de ajo
+        (11, 40, 1, 'Cucharadita(s)'),      --Orégano
+        (11, 313, 1, 'Cucharadita(s)'),     --Aceite de oliva
+        (11, 116, 400, 'Gramos'),           --Queso mozzarella
+        (11, 39, 1, 'Pieza(s)'),            --Albahaca        
+
+        (12, 145, 200, 'Gramos'),           --Harina de trigo
+        (12, 305, 100, 'Gramos'),           --Mantequilla
+        (12, 235, 100, 'Gramos'),           --Azúcar
+        (12, 304, 1, 'Pieza(s)'),           --Huevo
+        (12, 303, 1, 'Pizca(s)'),           --Sal
+        (12, 56, 5, 'Pieza(s)'),            --Manzana
+        (12, 250, 1, 'Cucharadita(s)'),     --Canela
+        (12, 253, 1, 'Pizca(s)'),           --Nuez moscada
+        (12, 327, 1, 'Cucharada(s)'),       --Jugo de limón
+        (12, 328, 2, 'Cucharada(s)'),       --Mermelada de albaricoque
+        (12, 276, 1, 'Cucharada(s)'),       --Agua
+
+        (13, 272, 500, 'Gramos'),           --Camarones
+        (13, 327, 0.5, 'Taza(s)'),          --Jugo de limón
+        (13, 329, 0.5, 'Taza(s)'),          --Jugo de limón verde
+        (13, 331, 1, 'Pieza(s)'),           --Cebolla roja
+        (13, 330, 1, 'Pieza(s)'),           --Jalapeño
+        (13, 15, 1, 'Pieza(s)'),            --Tomate
+        (13, 77, 1, 'Pieza(s)'),            --Aguacate
+        (13, 37, 0.25, 'Taza(s)'),          --Cilantro
+        (13, 303, 1, 'Cucharadita(s)'),     --Sal
+        (13, 310, 0.5, 'Cucharadita(s)'),   --Pimienta negra molida
+
+        (14, 334, 500, 'Gramos'),           --Pechuga de pollo molida
+        (14, 304, 1, 'Pieza(s)'),           --Huevo
+        (14, 333, 0.25, 'Taza(s)'),         --Pan rallado
+        (14, 7, 0.25, 'Taza(s)'),           --Cebolla
+        (14, 308, 2, 'Pieza(s)'),           --Dientes de ajo
+        (14, 36, 0.25, 'Taza(s)'),          --Perejil
+        (14, 303, 1, 'Cucharadita(s)'),     --Sal
+        (14, 310, 0.5, 'Cucharadita(s)'),   --Pimienta negra molida
+        (14, 332, 4, 'Pieza(s)'),           --Pan de hamburguesa
+
+        (15, 313, 2, 'Cucharada(s)'),       --Aceite de oliva
+        (15, 7, 1, 'Pieza(s)'),             --Cebolla
+        (15, 308, 3, 'Pieza(s)'),           --Dientes de ajo
+        (15, 1, 2, 'Pieza(s)'),             --Zanahoria
+        (15, 14, 2, 'Pieza(s)'),            --Apio
+        (15, 5, 1, 'Pieza(s)'),             --Pimiento rojo
+        (15, 294, 1, 'Taza(s)'),            --Lentejas secas
+        (15, 309, 8, 'Taza(s)'),            --Caldo de verduras
+        (15, 335, 1, 'Pieza(s)'),           --Lata de tomates en cubitos
+        (15, 45, 1, 'Cucharadita(s)'),      --Tomillo
+        (15, 311, 1, 'Cucharadita(s)'),     --Comino molido
+        (15, 321, 1, 'Taza(s)'),            --Hoja de laurel
+        (15, 3, 2, 'Taza(s)'),              --Espinacas
+
+        (16, 322, 12, 'Pieza(s)'),          --Tortilla de maíz
+        (16, 336, 2, 'Pieza(s)'),           --Pechuga de pollo cocida
+        (16, 337, 1, 'Taza(s)'),            --Queso rallado
+        (16, 338, 450, 'Gramos'),           --Tomatillo
+        (16, 330, 1, 'Pieza(s)'),           --Jalapeño
+        (16, 308, 1, 'Pieza(s)'),           --Diente de ajo
+        (16, 7, 0.5, 'Pieza(s)'),           --Cebolla
+        (16, 37, 0.5, 'Taza(s)'),           --Cilantro
+        (16, 327, 1, 'Cucharada(s)'),       --Jugo de limón
+        (16, 303, 1, 'Cucharadita(s)'),     --Sal
+
+        (17, 1, 2, 'Taza(s)'),              --Zanahoria
+        (17, 339, 1, 'Taza(s)'),            --Azúcar granulada
+        (17, 340, 0.5, 'Taza(s)'),          --Aceite vegetal
+        (17, 304, 4, 'Pieza(s)'),           --Huevo
+        (17, 145, 2, 'Taza(s)'),            --Harina de trigo
+        (17, 301, 2, 'Cucharadita(s)'),     --Polvo para hornear
+        (17, 302, 0.5, 'Cucharadita(s)'),   --Bicarbonato de sodio
+        (17, 303, 0.5, 'Cucharadita(s)'),   --Sal
+        (17, 250, 2, 'Cucharadita(s)'),     --Canela
+
+        (18, 263, 150, 'Gramos'),           --Atún
+        (18, 341, 0.5, 'Taza(s)'),          --Mayonesa
+        (18, 342, 1, 'Cucharada(s)'),       --Mostaza
+        (18, 327, 1, 'Cucharada(s)'),       --Jugo de limón
+        (18, 331, 0.25, 'Pieza(s)'),        --Cebolla roja
+        (18, 14, 1, 'Pieza(s)'),            --Apio
+        (18, 343, 0.25, 'Taza(s)'),         --Pepinillos
+        (18, 344, 0.25, 'Taza(s)'),         --Aceitunas negras
+        (18, 11, 2, 'Taza(s)'),             --Lechuga
+
+        (19, 325, 400, 'Gramos'),           --Pasta
+        (19, 304, 4, 'Pieza(s)'),           --Huevo
+        (19, 214, 200, 'Gramos'),           --Tocino
+        (19, 117, 0.5, 'Taza(s)'),          --Queso parmesano
+        (19, 308, 5, 'Pieza(s)'),           --Dientes de ajo
+
+        (20, 64, 1, 'Pieza(s)'),            --Mango
+        (20, 314, 0.5, 'Taza(s)'),          --Yogur natural
+        (20, 112, 0.5, 'Taza(s)'),          --Leche
+        (20, 239, 1, 'Cucharada(s)'),       --Miel
+
+        (21, 345, 2, 'Taza(s)'),            --Arroz de sushi
+        (21, 276, 2.25, 'Taza(s)'),         --Agua
+        (21, 346, 0.25, 'Taza(s)'),         --Vinagre de arroz
+        (21, 235, 2, 'Cucharada(s)'),       --Azúcar
+        (21, 303, 1, 'Cucharadita(s)'),     --Sal
+        (21, 347, 1, 'Pieza(s)'),           --Filete de salmón
+        (21, 77, 1, 'Pieza(s)'),            --Aguacate
+        (21, 13, 1, 'Pieza(s)'),            --Pepino
+        (21, 348, 4, 'Pieza(s)'),           --Hojas de alga nori
+
+        (22, 270, 1, 'Pieza(s)'),           --Pulpo
+        (22, 327, 0.5, 'Taza(s)'),          --Jugo de limón
+        (22, 349, 0.5, 'Taza(s)'),          --Jugo de lima
+        (22, 331, 1, 'Pieza(s)'),           --Cebolla roja
+        (22, 5, 1, 'Pieza(s)'),             --Pimiento
+        (22, 37, 0.5, 'Taza(s)'),           --Cilantro
+        (22, 77, 1, 'Pieza(s)'),            --Aguacate
+        (22, 313, 2, 'Cucharada(s)'),       --Aceite de oliva
+
+        (23, 169, 1, 'Kilogramo(s)'),       --Carne de res
+        (23, 350, 1, 'Kilogramo(s)'),       --Chorizo argentino
+        (23, 351, 1, 'Kilogramo(s)'),       --Morcillas argentinas
+        (23, 352, 6, 'Pieza(s)'),           --Provoleta
+
+        (24, 353, 16, 'Pieza(s)'),          --Láminas de lasaña
+        (24, 153, 500, 'Gramos'),           --Carne molida de res
+        (24, 7, 1, 'Pieza(s)'),             --Cebolla
+        (24, 308, 3, 'Pieza(s)'),           --Dientes de ajo
+        (24, 1, 1, 'Pieza(s)'),             --Zanahoria
+        (24, 335, 1, 'Pieza(s)'),           --Lata de tomates
+        (24, 354, 400, 'Gramos'),           --Salsa de tomate
+        (24, 235, 2, 'Cucharadita(s)'),     --Azúcar
+        (24, 40, 1, 'Cucharadita(s)'),      --Orégano
+        (24, 39, 1, 'Cucharadita(s)'),      --Albahaca
+        (24, 45, 1, 'Cucharadita(s)'),      --Tomillo
+        (24, 310, 0.5, 'Cucharadita(s)'),   --Pimienta negra molida
+        (24, 303, 0.5, 'Cucharadita(s)'),   --Sal
+        (24, 126, 2, 'Taza(s)'),            --Queso ricotta
+        (24, 116, 2, 'Taza(s)'),            --Queso mozzarella
+        (24, 117, 1, 'Taza(s)'),            --Queso parmesano
+        (24, 304, 1, 'Pieza(s)'),           --Huevo
+        (24, 355, 0.25, 'Taza(s)'),         --Hojas de albahaca
+
+        (25, 178, 4, 'Pieza(s)'),           --Pechuga de pollo
+        (25, 145, 0.5, 'Taza(s)'),          --Harina de trigo
+        (25, 304, 2, 'Pieza(s)'),           --Huevo
+        (25, 333, 1, 'Taza(s)'),            --Pan rallado
+        (25, 354, 2, 'Taza(s)'),            --Salsa de tomate
+        (25, 116, 2, 'Taza(s)'),            --Queso mozzarella
+        (25, 117, 0.5, 'Taza(s)'),          --Queso parmesano
+
+        (26, 304, 6, 'Pieza(s)'),           --Huevo
+        (26, 235, 150, 'Gramos'),           --Azúcar
+        (26, 356, 500, 'Gramos'),           --Queso mascarpone
+        (26, 357, 300, 'Mililitros'),       --Café espresso
+        (26, 358, 150, 'Mililitros'),       --Brandy
+        (26, 359, 30, 'Pieza(s)'),          --Galletas de savoiardi
+
+        (27, 360, 450, 'Gramos'),           --Lomo de res
+        (27, 340, 4, 'Cucharada(s)'),       --Aceite vegetal
+        (27, 331, 1, 'Pieza(s)'),           --Cebolla roja
+        (27, 15, 2, 'Pieza(s)'),            --Tomate
+        (27, 361, 1, 'Pieza(s)'),           --Pimiento amarillo
+        (27, 5, 1, 'Pieza(s)'),             --Pimiento rojo
+        (27, 308, 3, 'Pieza(s)'),           --Dientes de ajo
+        (27, 362, 0.25, 'Taza(s)'),         --Salsa de soja
+        (27, 363, 0.25, 'Taza(s)'),         --Vinagre blanco
+        (27, 364, 2, 'Cucharadita(s)'),     --Ajil amarillo molido
+        (27, 37, 0.25, 'Taza(s)'),          --Cilantro
+
+        (28, 365, 500, 'Gramos'),           --Filete de pescado blanco
+        (28, 327, 0.25, 'Taza(s)'),         --Jugo de limón
+        (28, 113, 0.25, 'Taza(s)'),         --Yogur
+        (28, 341, 2, 'Pieza(s)'),           --Mayonesa
+        (28, 303, 1, 'Cucharadita(s)'),     --Sal
+        (28, 310, 1, 'Cucharadita(s)'),     --Pimienta negra molida
+        (28, 366, 1, 'Cucharadita(s)'),     --Paprika
+        (28, 311, 0.5, 'Cucharadita(s)'),   --Comino molido
+
+        (29, 367, 2, 'Taza(s)'),            --Arroz de Arborio
+        (29, 16, 10, 'Pieza(s)'),           --Champiñón
+        (29, 7, 1, 'Pieza(s)'),             --Cebolla
+        (29, 308, 3, 'Pieza(s)'),           --Dientes de ajo
+        (29, 309, 4, 'Taza(s)'),            --Caldo de verduras
+        (29, 285, 1, 'Taza(s)'),            --Vino blanco
+        (29, 117, 0.5, 'Taza(s)'),          --Queso parmesano
+        (29, 305, 4, 'Cucharada(s)'),       --Mantequilla
+        (29, 313, 2, 'Cucharada(s)'),       --Aceite de oliva
+
+        (30, 145, 500, 'Gramos'),           --Harina de trigo
+        (30, 276, 325, 'Mililitros'),       --Agua
+        (30, 318, 7, 'Gramos'),             --Levadura
+        (30, 303, 1, 'Cucharadita(s)'),     --Sal
+        (30, 235, 1, 'Cucharadita(s)'),     --Azúcar
+        (30, 313, 2, 'Cucharada(s)'),       --Aceite de oliva
+        (30, 354, 1, 'Taza(s)'),            --Salsa de tomate
+        (30, 116, 2, 'Taza(s)'),            --Queso mozzarella
+        (30, 368, 200, 'Gramos'),           --Jamón de pierna
+        (30, 63, 200, 'Gramos'),            --Piña
+        (30, 331, 0.5, 'Pieza(s)');         --Cebolla roja
